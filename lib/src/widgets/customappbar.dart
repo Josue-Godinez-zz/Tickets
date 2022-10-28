6017c0262cf6b961e086470a4764f8b6f835457d
Josue-Godinez <josuegod1998@gmail.com>
2022-10-27 20:27:16 -0600

    Missing file

---
+++ lib/src/widgets/customappbar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:venta_de_tickets/main.dart';
+import 'package:venta_de_tickets/src/views/cinemas/cinema.dart';
+import 'package:venta_de_tickets/src/views/landing/landing.dart';
+import 'package:venta_de_tickets/src/views/movie/movie.dart';
+
+// ignore: must_be_immutable
+class CustomAppBar extends StatefulWidget {
+  Areas returnArea;
+  String areaName;
+  CustomAppBar({Key? key, required this.returnArea, required this.areaName})
+      : super(key: key);
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBar();
+}
+
+enum Areas { landing, cinema, schedule, movie }
+
+class _CustomAppBar extends State<CustomAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+              switch (widget.returnArea) {
+                case Areas.landing:
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Landing()));
+                  break;
+                case Areas.cinema:
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Cinema()));
+                  break;
+                case Areas.schedule:
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Cinema()));
+                  break;
+                case Areas.movie:
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Movie()));
+                  break;
+              }
+            },
+            icon: const Icon(Icons.keyboard_arrow_left)),
+        Text(widget.areaName)
+      ],
+    );
+  }
+}
